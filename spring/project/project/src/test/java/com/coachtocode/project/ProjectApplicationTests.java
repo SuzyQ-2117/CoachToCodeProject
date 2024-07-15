890b1d19514de3d5a658c6401426c9f303b6abba
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-15 17:08:11 +0100

    Spring/Maven project files created

---
+++ spring/project/project/src/test/java/com/coachtocode/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachtocode.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
