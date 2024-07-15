3f665f52174e8081718635bf9ccba3beeb369a33
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-15 21:11:42 +0100

    Basic Navbar started

---
+++ frontend-react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+            </ul>
+        </div>
+    )
+}
