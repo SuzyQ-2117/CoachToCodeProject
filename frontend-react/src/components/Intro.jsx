674b3005bbc1ce262742fcf3c204a380bf495309
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-23 22:03:00 +0100

    placeholder text added in all intro divs

--- frontend-react/src/components/Intro.jsx
+++ frontend-react/src/components/Intro.jsx
@@ -1,78 +1,81 @@
-import suzy from '../media/suzy.jpg';
-
 export default function Intro() {
     return (
-        <div>
-            <div className="top-section">
-                <h2>Coach to Code 2024</h2>
+        <div className="container">
+
+
+            <div className="top-far-left">
+                <h4>Front-end Stack . . . . . . . . . . . </h4>
+                <p>HTML & CSS</p>
+                <p>Bootstrap</p>
+                <p>React</p>
+            </div>
+
+            <div className="top-left">
+                <h4>Back-end Stack . . . . . . . . . . .</h4>
+                <p>Java</p>
+                <p>Springboot</p>
+                <p>Maven</p>
+            </div>
+
+            <div className="top-right">
+                <h4>DevOps . . . . . . . . . . . . . . . . . . .</h4>
+                <p>CI/CD with Jenkins</p>
+                <p>Agile Methodology</p>
+                <p>Google Cloud Platform (GCP)</p>
+            </div>
+
+            <div className="top-far-right">
+                <h4>PLACEHOLDER TEXT . . . . . . . </h4>
+                <p>PLACEHOLDER TEXT</p>
+                <p>PLACEHOLDER TEXT</p>
+                <p>PLACEHOLDER TEXT</p>
             </div>
 
 
-
-
-            <div class="container">
-
-                <div class="top-left">
-                    <h1>Hello Internet!</h1>
-                    <h2>I'm Suzy Q!</h2>
-                    <br/>
-                    <p>I'm an aspiring Software Engineer! After 11 years in customer-facing and B2B roles, I've recently embarked on a complete career changed, facilitated by our wonderful Reskilling Team.</p>
-                    <br/>
-                    <p>This is some more information about me. I have a dog and a partner of 10 years - we live together in sunny South Wales (LOL IT'S NEVER SUNNY HERE).</p>
-                </div>
-
-                <div class="top-right">
-                    <p>This was originally going to be a complete copy & paste of the first section so that I could see what the layout looked like, but I decided that would look silly and actually wouldn't give me the look I was trying to visualise - especially as my project will look like this current example layout.</p>
-                    <br/>
-                    <p>HTML was the first programming language I learned. I began learning HTML way back in 2017 so that I could build a custom Hive page for my department at the time. That was really when I rediscovered my love for the logic that coding embodies. </p>
-                </div>
-
-
-                <div class="mid-far-left">
-                    <h4>Front-end Stack . . . . . . . . </h4>
-                    <p>HTML & CSS</p>
-                    <p>Bootstrap</p>
-                    <p>React</p>
-                </div>
-
-                <div class="mid-left">
-                    <h4>Back-end Stack . . . . . . . . . </h4>
-                    <p>Java</p>
-                    <p>Springboot</p>
-                    <p>Maven</p>
-                </div>
-
-                <div class="mid-right">
-                    <h4>DevOps . . . . . . . . . . . . . . . . </h4>
-                    <p>CI/CD with Jenkins</p>
-                    <p>Agile Methodology</p>
-                    <p>Google Cloud Platform (GCP)</p>
-                </div>
-
-                <div class="mid-far-right">
-                    <p>PLACEHOLDER TEXT</p>
-                </div>
-
-
-                <div class="bottom-left">
-                    <p>I'm going to write a bunch of stuff in here about who I am and what my experience is.</p>
-                </div>
-
-                <div class="bottom-middle">
-                    <p>PLACEHOLDER TEXT</p>
-                </div>
-
-                <div class="bottom-right">
-                    <p>It might be worth outlining a brief summary of my career history as this is for interenal use, but remember this will be public on GitHub</p>
-                </div>
+            <div className="mid-left">
+                <h1>Hello Internet!</h1>
+                <h2>I'm Suzy Q.</h2>
+                <br />
+                <p>I'm an aspiring Software Engineer! After 11 years in customer-facing and B2B roles, I've recently embarked on a complete career changed, facilitated by our wonderful Reskilling Team.</p>
+                <br />
+                <p>This is some more information about me. I have a 2-year-old black German Shepherd and my partner of 10 years & I live together in sunny South Wales (LOL IT'S NEVER SUNNY HERE).</p>
 
             </div>
 
+            <div className="mid-right">
+                <p>This was originally going to be a complete copy & paste of the first section so that I could see what the layout looked like, but I decided that would look silly and actually wouldn't give me the look I was trying to visualise - especially as my project will look like this current example layout.</p>
+                <br />
+                <p>HTML was the first programming language I learned. I began learning HTML way back in 2017 so that I could build a custom Hive page for my department at the time. That was really when I rediscovered my love for the logic that coding embodies. </p>
+            </div>
 
 
+            <div className="bottom-left">
+                <p>I'm going to write a bunch of stuff in here about who I am and what my experience is.</p>
+                <br />
+                <p>It might be worth outlining a brief summary of my career history as this is for interenal use, but remember this will be public on GitHub</p>
+            </div>
 
+            <div className="bottom-middle">
+                <p>I could really do with writing a few more words in this section so that I can see what the page layout will look like. </p>
+                <br />
+                <p>I honestly have no idea what to expect going into my new lab. I've never worked in the Tech industry before so this is all new to me. I'm very excited to get started though!</p>
+                <br />
+                <p>I'm going to just keep writing words here. This whole page is looking a little bit death-by-words so I should probably add some images in here somewhere.</p>
+                <br />
+                <p>I don't enjoy the wall of words aesthetic!</p>
+            </div>
 
-
+            <div className="bottom-right">
+                <h4>Career</h4>
+                <ul>
+                    <li>Telephone Banking Assistant</li>
+                    <li>Connect Team Manager</li>
+                    <li>Bereavement Specialist</li>
+                    <li>Banking Consultant</li>
+                    <li>Business Development Manager</li>
+                    <li>Software Engineer</li>
+                </ul>
+            </div>
 
         </div>
     )
