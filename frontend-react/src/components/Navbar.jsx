306f1b4d5e590e8d4456e6e233a51c8982b798a1
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-23 22:03:29 +0100

    selfie moved to navbar

--- frontend-react/src/components/Navbar.jsx
+++ frontend-react/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom"
+import suzy from '../media/suzy.jpg';
 
 export default function Navbar() {
     return (
         <div className="sidebar">
+            <img src={suzy} className="selfie" alt="The project creator" />
+            <br />
             <h3>Suzy Brown</h3>
+            <br />
             <h6>Trainee Software Engineer</h6>
-            <br/>
+            <br />
             <p className="border-bottom">I'm currently training as part of a career reskill! I'll be working in the Devices Lab</p>
             <ul>
                 <li>
