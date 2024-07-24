267b132b46761c1018b872703b983515c74f3fc0
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-24 22:12:08 +0100

    Bootstrap CSS import link added but commented out as too many unwanted styles

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
