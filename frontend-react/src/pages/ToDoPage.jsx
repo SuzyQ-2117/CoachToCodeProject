e6960273e62ff8eaa623805f0debdac72a197459
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-25 23:57:35 +0100

    Edit modal now called directly from ToDoPage to reduce unnecessary prop drilling.

--- frontend-react/src/pages/ToDoPage.jsx
+++ frontend-react/src/pages/ToDoPage.jsx
@@ -1,16 +1,36 @@
+import { useState, useEffect } from "react";
 import AddToDoModal from "../components/AddToDoModal";
+import EditToDoModal from "../components/EditToDoModal";
 import ToDoCard from "../components/ToDoCard";
 import { url } from "../components/url";
-import { useState, useEffect } from "react";
 
 export default function ToDoPage() {
-    const [show, setShow] = useState(false);
-    let [apiData, setData] = useState([]);
+    const [showAdd, setShowAdd] = useState(false);
+    const [showEdit, setShowEdit] = useState(false);
+    const [apiData, setData] = useState([]);
 
-    const handleShow = () => setShow(true);
+    const [updatedId, setUpdatedId] = useState();
+    const [updatedTitle, setUpdatedTitle] = useState('');
+    const [updatedDescription, setUpdatedDescription] = useState('');
+    const [updatedDueDate, setUpdatedDueDate] = useState('');
+    const [updatedDueTime, setUpdatedDueTime] = useState('');
+    const [updatedStatus, setUpdatedStatus] = useState('');
+
+    const handleShowAddModal = () => setShowAdd(true);
+
+    const handleShowEditModal = (task) => {
+        setUpdatedId(task.id);
+        setUpdatedTitle(task.title);
+        setUpdatedDescription(task.description);
+        setUpdatedDueDate(task.dueDate);
+        setUpdatedDueTime(task.dueTime);
+        setUpdatedStatus(task.status);
+        setShowEdit(true);
+    };
+
+    const handleCloseEditModal = () => setShowEdit(false);
 
     function fetchData() {
-        console.log("FETCHING")
         fetch(`${url}/task/all`)
             .then((response) => response.json())
             .then((data) => setData(data));
@@ -20,32 +40,50 @@ export default function ToDoPage() {
         fetchData();
     }, []);
 
-    // will need to perform a fetch request here to retrieve the details of all tasks held in the database and will then need to deconstruct them and parse the data into the ToDoCard
-
     return (
         <div className="coloured-background">
-            <h3>This is a heading about the tasks on this page!</h3>
-            <br />
-            <p>This is the section where my fancy To Do app will be displayed</p>
-            <br />
-            <div>
-                <button variant="primary" onClick={handleShow}>
+            <div className="todo-headings">
+                <h3>This is a heading about the tasks on this page!</h3>
+                <br />
+                <p>This is the section where my fancy To Do app will be displayed</p>
+                <br />
+                <button className="add-btn" onClick={handleShowAddModal}>
                     Add new Task!
                 </button>
             </div>
-            <div>
+            <div className="flex flex-wrap">
                 {apiData.map((task) => (
-                    <ToDoCard key={task.id}
+                    <ToDoCard
+                        key={task.id}
+                        id={task.id}
                         title={task.title}
                         description={task.description}
                         dueDate={task.dueDate}
                         dueTime={task.dueTime}
                         status={task.status}
+                        fetchData={fetchData}
+                        onEdit={() => handleShowEditModal(task)} // Pass the task to the handleShowEditModal function
                     />
-                ))
-                }
+                ))}
             </div>
-            <AddToDoModal show={show} setShow={setShow} fetchData={fetchData}/>
+            <AddToDoModal showAdd={showAdd} setShowAdd={setShowAdd} fetchData={fetchData} />
+            <EditToDoModal
+                showEdit={showEdit}
+                setShowEdit={setShowEdit}
+                fetchData={fetchData}
+                updatedId={updatedId}
+                setUpdatedId={setUpdatedId}
+                updatedTitle={updatedTitle}
+                setUpdatedTitle={setUpdatedTitle}
+                updatedDescription={updatedDescription}
+                setUpdatedDescription={setUpdatedDescription}
+                updatedDueDate={updatedDueDate}
+                setUpdatedDueDate={setUpdatedDueDate}
+                updatedDueTime={updatedDueTime}
+                setUpdatedDueTime={setUpdatedDueTime}
+                updatedStatus={updatedStatus}
+                setUpdatedStatus={setUpdatedStatus}
+            />
         </div>
     )
 }
