29ac281b6fb538b1cfa09586048dcf593f25fc74
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-31 12:18:01 +0100

    Reorganised Grid layout, re-wrote placeholder text, started to add images to the front-end stack coloured box to represent technologies used

--- frontend-react/src/components/Intro.jsx
+++ frontend-react/src/components/Intro.jsx
@@ -1,38 +1,12 @@
+import htmlLogo from '../media/javascript-39396.png';
+import reactLogo from '../media/react-logo.png'
+import guardDog from '../media/IMG_4146.JPG';
+
 export default function Intro() {
     return (
         <div className="container coloured-background">
 
-
-            <div className="top-far-left">
-                <h4>Front-end Stack . . . . . . . . . . . </h4>
-                <p>HTML & CSS</p>
-                <p>Bootstrap</p>
-                <p>React</p>
-            </div>
-
-            <div className="top-left">
-                <h4>Back-end Stack . . . . . . . . . . .</h4>
-                <p>Java</p>
-                <p>Springboot</p>
-                <p>Maven</p>
-            </div>
-
-            <div className="top-right">
-                <h4>DevOps . . . . . . . . . . . . . . . . . . .</h4>
-                <p>CI/CD with Jenkins</p>
-                <p>Agile Methodology</p>
-                <p>Google Cloud Platform (GCP)</p>
-            </div>
-
-            <div className="top-far-right">
-                <h4>PLACEHOLDER TEXT . . . . . . . </h4>
-                <p>PLACEHOLDER TEXT</p>
-                <p>PLACEHOLDER TEXT</p>
-                <p>PLACEHOLDER TEXT</p>
-            </div>
-
-
-            <div className="mid-left">
+            <div className="top-line">
                 <h1>Hello Internet!</h1>
                 <h2>I'm Suzy Q.</h2>
                 <br />
@@ -42,39 +16,76 @@ export default function Intro() {
 
             </div>
 
-            <div className="mid-right">
+            <div className="top-far-left">
+                <h4>Front-end Stack . . . . . . . . . </h4>
+                <br /> 
+                <img src={htmlLogo} alt="Logos for HTML, CSS and JavaScript" />
+                <img src={reactLogo} alt="Logo for React" />
+                <p>Bootstrap</p>
+            </div>
+
+            <div className="top-left">
+                <h4>Back-end Stack . . . . . . . . . .</h4>
+                <p>Java</p>
+                <p>Springboot</p>
+                <p>Maven</p>
+            </div>
+
+            <div className="top-right">
+                <h4>DevOps . . . . . . . . . . . . . . . . .</h4>
+                <p>CI/CD with Jenkins</p>
+                <p>Agile Methodology</p>
+                <p>Google Cloud Platform (GCP)</p>
+            </div>
+
+            <div className="top-far-right">
+                <h4>PLACEHOLDER TEXT . . . . . </h4>
+                <p>PLACEHOLDER TEXT</p>
+                <p>PLACEHOLDER TEXT</p>
+                <p>PLACEHOLDER TEXT</p>
+            </div>
+
+            <div className="mid-line">
                 <p>This was originally going to be a complete copy & paste of the first section so that I could see what the layout looked like, but I decided that would look silly and actually wouldn't give me the look I was trying to visualise - especially as my project will look like this current example layout.</p>
                 <br />
                 <p>HTML was the first programming language I learned. I began learning HTML way back in 2017 so that I could build a custom Hive page for my department at the time. That was really when I rediscovered my love for the logic that coding embodies. </p>
             </div>
 
-
             <div className="bottom-left">
-                <p>I'm going to write a bunch of stuff in here about who I am and what my experience is.</p>
+                <h4>Storm</h4>
+                <p>My partner & I have a German Shepherd called Storm - he is our absolute pride and joy. He's two years old and is still so full of puppy energy!</p>
                 <br />
-                <p>It might be worth outlining a brief summary of my career history as this is for interenal use, but remember this will be public on GitHub</p>
+                <img src={guardDog} alt="Storm takes his Guard Dog Duties seriously" className="storm" />
             </div>
 
-            <div className="bottom-middle">
-                <p>I could really do with writing a few more words in this section so that I can see what the page layout will look like. </p>
+            <div className="bottom-mid">
+                <h4>Career Highlights</h4>
                 <br />
-                <p>I honestly have no idea what to expect going into my new lab. I've never worked in the Tech industry before so this is all new to me. I'm very excited to get started though!</p>
+                <p>2013: Telephone Banking Customer Assistant</p>
+                <p>Worked in a variety of teams and on a variety of shift patterns supporting customers with their Telephony queries. This also included facilitating inductions for new staff members as well as a brief period of Team Management.</p>
                 <br />
-                <p>I'm going to just keep writing words here. This whole page is looking a little bit death-by-words so I should probably add some images in here somewhere.</p>
+                <p>2017: Achieved A-Level Maths</p>
+                <p>Balancing full-time work and a part-time college course was tough, but I managed to complete all parts of my course and achieved a C at A-Level in Mathematics.</p>
                 <br />
-                <p>I don't enjoy the wall of words aesthetic!</p>
+                <p>2017: MOT Support Specialist</p>
+                <p>Moved into the Moments of Truth (MOT) Team providing industry-leading support to customers. I provided support through Bereavement, Financial Difficulties, Cancer Diagnoses, Representative Access requests and specialist needs.</p>
+                <br />
+                <p>2018: Banking Consultant</p>
+                <p>Worked across 5 different branches providing Banking, Savings and Lending support to customers with a variety of needs. </p>
+                <br />
+                <p>2020: Achieved CeMAP</p>
+                <p></p>
+                <br />
+                <p>2021: Business Development Manager</p>
+                <p></p>
+                <br />
+                <p>2023: Achieved CeRER</p>
+                <p></p>
+                <br />
             </div>
 
             <div className="bottom-right">
-                <h4>Career</h4>
-                <ul>
-                    <li>Telephone Banking Assistant</li>
-                    <li>Connect Team Manager</li>
-                    <li>Bereavement Specialist</li>
-                    <li>Banking Consultant</li>
-                    <li>Business Development Manager</li>
-                    <li>Software Engineer</li>
-                </ul>
+                
             </div>
 
         </div>
